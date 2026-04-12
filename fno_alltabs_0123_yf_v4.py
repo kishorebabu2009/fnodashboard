6704513986b9df6bb74565766b80230aab66e41f
kishorebabu2009 <kishorebabu2009@gmail.com>
2026-04-13 04:09:51 +0530

    Update fno_alltabs_0123_yf_v4.py

--- fno_alltabs_0123_yf_v4.py
+++ fno_alltabs_0123_yf_v4.py
@@ -235,8 +235,8 @@ with st.sidebar:
                 if curr_c > ma20.iloc[-1]: s1 += 10
                 if curr_c > ma50.iloc[-1]: s1 += 10
                 if ma200 is not None and hasattr(ma200, "iloc") and len(ma200.dropna()) > 0:
-                if curr_c > ma200.dropna().iloc[-1]: s1 += 10
-                # if curr_c > ma200.iloc[-1]: s1 += 10
+                    if curr_c > ma200.dropna().iloc[-1]: s1 += 10
+                    # if curr_c > ma200.iloc[-1]: s1 += 10
                 if ma20.iloc[-1] > ma50.iloc[-1] > ma200.iloc[-1]: s1 += 10 # Golden Alignment bonus
                 s1 = min(s1, 40) # Cap at 40
 
@@ -497,5 +497,6 @@ if df is not None:
 
 else:
     st.info("System Standby. Execute Market Scan to activate modules.")
+
 
 
